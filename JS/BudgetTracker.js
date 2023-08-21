93be20733aca3411defe55dc547f832f51c559d4
CquashaunG <cgrantham910@gmail.com>
2023-08-21 15:37:34 -0400

    add JS code and stylesheet

---
+++ JS/BudgetTracker.js
@@ -0,0 +1,41 @@
+export default class BudgetTracker {
+    constructor(querySelectorString) {
+
+    }
+
+    static html() {
+
+    }
+
+    static entryHtml() {
+
+    }
+
+    load() {
+
+    }
+
+    updateSummary() {
+
+    }
+
+    save() {
+
+    }
+
+    addEntry(entry = {}) {
+
+    }
+
+    getEntryRows() {
+
+    }
+
+    onNewEntryBtnClick() {
+
+    }
+
+    onDeleteEntryBtnClick(e) {
+        
+    }
+}
